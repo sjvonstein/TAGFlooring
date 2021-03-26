77099a2c85aad8d85b83e0e7a57cd61d18e5a223
Stefan VonStein <sjvonstein@gmail.com>
2021-03-26 14:34:04 -0400

    trying to sort out issue with navbar dropdown only working once. Suspecting the sprockets require lines

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//following install instructions for bootstrap gem, the next 3 lines should be commented out; however, dropdown in navbar
-//stops working properly if I do. Turns out it won't compiled on heroku because sprokets uses
-//the comment slashes with an equals sign
 
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
